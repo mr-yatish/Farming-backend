6f12d371fc1da27bf8a318bd29d197f7b9a1a4f9
yatishprajapatbrandshow <yatish.prajapat@brandshow.in>
2025-03-27 19:47:14 +0530

    Delete Api Code

--- routes/Record.router.js
+++ routes/Record.router.js
@@ -6,6 +6,6 @@ const recordRouter = require("../controller/Record.controller");
 
 router.post("/create", recordRouter.createRecord);
 router.get("/getAllRecords", recordRouter.getAll);
-router.delete("/deleteRecord", recordRouter.deleteRecord);
+router.delete("/deleteRecord/:id", recordRouter.deleteRecord);
 // Export the router
 module.exports = router;
